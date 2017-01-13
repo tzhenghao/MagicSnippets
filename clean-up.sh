b119bb28179173b5257b73ac897b50a1316bcf5b
tzhenghao <tanzhao@umich.edu>
2017-01-13 16:15:28 -0600

    added zsh cleanup

--- clean-up.sh
+++ clean-up.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Author: Zheng Hao Tan
 # Email: tanzhao@umich.edu
 
-echo "clean up shell history..."
-history -c
-echo "cleaned up shell history!"
+echo "clean up zsh history..."
+rm ~/.zsh_history
+echo "cleaned up zsh history!"
