f207f952021476ad29871ea3e3e377636c237fc3
tzhenghao <tanzhao@umich.edu>
2017-03-26 01:11:10 -0500

    added travis yml file and shellcheck

---
+++ update-arcanist.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Author: Zheng Hao Tan
+# Email: tanzhao@umich.edu
+
+(cd ~/toolchains/arcanist && git pull)
--- update-vim-packages.sh
+++ update-vim-packages.sh
@@ -3,13 +3,8 @@
 # Author: Zheng Hao Tan
 # Email: tanzhao@umich.edu
 
-cd ~/.vim/bundle/nerdcommenter && git pull
-cd -
-cd ~/.vim/bundle/vim-airline && git pull
-cd -
-cd ~/.vim/bundle/vim-gitgutter && git pull
-cd -
-cd ~/.vim/bundle/neocomplete.vim && git pull
-cd -
-cd ~/.vim/bundle/vim-colors-solarized && git pull
-cd -
+(cd ~/.vim/bundle/nerdcommenter && git pull)
+(cd ~/.vim/bundle/vim-airline && git pull)
+(cd ~/.vim/bundle/vim-gitgutter && git pull)
+(cd ~/.vim/bundle/neocomplete.vim && git pull)
+(cd ~/.vim/bundle/vim-colors-solarized && git pull)
