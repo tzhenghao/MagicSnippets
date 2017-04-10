f59627e408b75f8f792d34993c39e3fbac848960
tzhenghao <tanzhao@umich.edu>
2017-04-09 23:43:18 -0500

    added help command and cleanup function

--- reminder.sh
+++ reminder.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
+#/ Usage: reminder.sh <sleepTime> <message>
+#/ Sets a timer of sleepTime seconds with the given message.
+usage() {
+    grep '^#/' "$0" | cut -c4-
+    exit 0
+}
+
+expr "$*" : ".*--help" > /dev/null && usage
+
 sleepTime="$1"
 message="$2"
 
 (sleep "${sleepTime:?}"; echo "${message:?}") &
+
+cleanup() {
+  true;
+}
+trap cleanup EXIT
